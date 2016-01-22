1aded4a2577d7fffc318ea7a0d1d1c90dab02b9b
mcbelte <thomasevang@gmail.com>
2016-01-22 14:05:00 +0100

    tcp server listening for incoming connections

--- Exercise3/src/TCP/tcpServer.c
+++ Exercise3/src/TCP/tcpServer.c
@@ -4,17 +4,51 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<pthread.h>
+
+void* recieveFromClient(void *news)
+{
+  printf("Connection Recieved\n");
+  char *msg = "Hide yo wife!";
+  int len, bytes_sent, i;
+
+  len = strlen(msg);
+  printf("I am thread\n");
+
+  while(i < 10)
+  {
+
+  if((bytes_sent = send( *(int*)news, msg, len, 0)) == -1)
+  {
+    printf("Failed to send Message");
+  }
+  else
+  {
+    printf("Sending Message %d\n", (i+1));
+  }
+
+  sleep(1);
+  i++;
+
+  }
+
+  close(*(int*) news);
+
+  return NULL;
+}
+
 
 #define BACKLOG 10
 
 int main(int argc, char const *argv[]) {
 
+  pthread_t recieveThread;
   struct sockaddr_storage their_addr;
-  socklen_t addr_size;
   struct addrinfo hints;
   struct addrinfo *serverinfo;
   int s, news;
   int status;
+  socklen_t addr_size;
 
   memset(&hints, 0, sizeof hints);
 
@@ -23,7 +57,7 @@ int main(int argc, char const *argv[]) {
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
 
-  if((status = getaddrinfo(NULL, "34000", &hints, &serverinfo)) != 0)
+  if((status = getaddrinfo(NULL, "35178", &hints, &serverinfo)) != 0)
   {
     printf("getaddrinfo error\n");
   }
@@ -36,11 +70,19 @@ int main(int argc, char const *argv[]) {
 
   bind(s, serverinfo->ai_addr, serverinfo->ai_addrlen);
 
-  printf("Hello\n");
+  while(1)
+  {
+
+  printf("Listening for connections...\n");
   listen(s, BACKLOG);
-  printf("I has connection");
+
   addr_size = sizeof their_addr;
   news = accept(s, (struct sockaddr *)&their_addr, &addr_size);
+
+  pthread_create(&recieveThread, NULL , recieveFromClient, &news);
+
+  }
+
 
   return 0;
 
