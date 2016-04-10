3b859d75dd80924878ffba71cdb5a5d3bd4538bf
mcbelte <thomasevang@gmail.com>
2016-04-10 13:22:14 +0200

    lights

---
+++ Making_Modules/basic_light_functions.h
@@ -0,0 +1,6 @@
+#include "elev.h"
+
+int inner_button_light_switch(int floor, int value);
+int button_up_light_switch(int floor, int type);
+int button_down_light_switch(int floor, int value);
+int update_floor_indicator(int floor);
